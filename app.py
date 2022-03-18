9dee1940165123aef5cd70e57a751da32dd90650
DENN87 <denis.schiopu@yahoo.com>
2022-03-18 13:06:33 -0700

    Defined endpoints to be implemented.

--- app.py
+++ app.py
@@ -2,9 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# GET /store
+# GET /store/<string:name>
+# POST /store data: {name:, items:}
+# GET /store/<string:name>/items
+# POST /store/<string:name>/item {name:, price:}
+
 @app.route('/')
 def home():
-   return 'Home'
+    return 'Home'
+
 
 if __name__ == __name__:
-   app.run(port=3000)
+    app.run(port=3000)
