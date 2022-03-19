c311091ac5525b413015cb3266490e586340af45
DENN87 <denis.schiopu@yahoo.com>
2022-03-18 18:13:16 -0700

    Improved code and error control.

--- RESTful app.py
+++ RESTful app.py
@@ -1,16 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 api = Api(app)
 
-# every Resource must be a class
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
-    
-    
-api.add_resource(Student, '/student/<string:name>')  # http://127.0.0.1:5000/student/Test
+items = []
 
-app.run(port=5000)
+class Item(Resource):
+    def get(self, name):
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return item, 200 if item else 404
+    
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {f'message': "An item with name {name} already exists."}, 400
+        else:
+            item = request.get_json()
+            items.append(item)
+            return item, 201
+    
+        
+class ItemList(Resource):
+    def get(self):
+        return items
+        
+    
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+
+app.run(port=5000, debug=True)
 
