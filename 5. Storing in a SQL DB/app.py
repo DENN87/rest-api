8b5728faaf268aa8a955c934af534c7421cdaa3e
DENN87 <denis.schiopu@yahoo.com>
2022-03-23 16:44:34 -0700

    Signing up and writing new Users to a database. Created table schema.

--- 5. Storing in a SQL DB/app.py
+++ 5. Storing in a SQL DB/app.py
@@ -4,6 +4,8 @@ from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
 
+from user import UserRegister
+
 app = Flask(__name__)
 app.secret_key = 'try'
 api = Api(app)
@@ -62,5 +64,6 @@ class ItemList(Resource):
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 app.run(port=5000, debug=True)
