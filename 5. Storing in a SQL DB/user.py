bff75a671b861e4131f408f6fe4c73bec5963b7f
DENN87 <denis.schiopu@yahoo.com>
2022-03-23 18:52:11 -0700

    JWT added configurations, URL Rule and Token Expiration along with JWT error handler and response handler.

--- 5. Storing in a SQL DB/user.py
+++ 5. Storing in a SQL DB/user.py
@@ -2,12 +2,13 @@ import sqlite3
 
 from flask_restful import Resource, reqparse
 
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id,
         self.username = username,
         self.password = password
-
+    
     # retrieve user object by username
     @classmethod
     def find_by_username(cls, username):
@@ -24,7 +25,7 @@ class User:
             user = cls(*row)  # *row = row[0], row[1], row[2] = columns id, username, password
         else:
             user = None
-            
+        
         connection.close()
         return user
     
@@ -41,29 +42,30 @@ class User:
             user = cls(*row)
         else:
             user = None
-            
+        
         connection.close()
         return user
+
 
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
     parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank."
+                        type = str,
+                        required = True,
+                        help = "This field cannot be blank."
                         )
     parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank."
+                        type = str,
+                        required = True,
+                        help = "This field cannot be blank."
                         )
-
+    
     def post(self):
         data = UserRegister.parser.parse_args()
-
+        
         if User.find_by_username(data['username']):
             return {"message": "A user with that username already exists."}, 400
-
+        
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         
@@ -74,4 +76,11 @@ class UserRegister(Resource):
         connection.close()
         
         return {"message": "User created successfully."}, 201
-    
+
+# TO IMPLEMENT FOR ADMIN USER ONLY
+# from flask_jwt import jwt_required, current_identity
+# class User(Resource):
+#     @jwt_required()
+#     def get(self):   # view all users
+#         user = current_identity
+#         # then implement admin auth method
