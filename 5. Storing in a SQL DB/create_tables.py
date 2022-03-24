2ccf89bcf91cafb7f738ae5f18d1faf48835ba93
DENN87 <denis.schiopu@yahoo.com>
2022-03-23 17:07:18 -0700

    Refactored Flask app. Implemented GET item from DB in Class Item.

--- 5. Storing in a SQL DB/create_tables.py
+++ 5. Storing in a SQL DB/create_tables.py
@@ -7,6 +7,11 @@ create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, userna
 # id INTEGER PRIMARY KEY - will be assigned automatically by DB and autoincrement for every new user created
 cursor.execute(create_table)
 
+create_table = "CREATE TABLE IF NOT EXISTS items (name text, price real)"
+cursor.execute(create_table)
+
+cursor.execute("INSERT INTO items VALUES ('new-item', 10.99)")
+
 connection.commit()
 
 connection.close()
---
+++ 5. Storing in a SQL DB/item.py
@@ -0,0 +1,60 @@
+import sqlite3
+from flask_jwt import jwt_required
+from flask_restful import Resource, reqparse
+
+
+class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+                        type = float,
+                        required = True,
+                        help = "This field cannot be blank!"
+                        )
+    
+    @jwt_required()
+    def get(self, name):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        query = "SELECT * FROM items WHERE name=?"
+        result = cursor.execute(query, (name,))
+        row = result.fetchone()
+        connection.close()
+        
+        if row:
+            return {'item': {'name': row[0], 'price': row[1]}}
+
+        return {'message': 'Item not found.'}, 404
+        
+    def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return {f'message': "An item with name {name} already exists."}, 400
+        
+        data = Item.parser.parse_args()
+        
+        item = {'name': name, 'price': data['price']}
+        items.append(item)
+        return item, 201
+    
+    def delete(self, name):
+        global items
+        # getting all the items OTHER THAN THE ONE TO BE DELETED
+        # and saving to the global items list
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message': 'Item deleted'}
+    
+    def put(self, name):
+        data = Item.parser.parse_args()
+        
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
+
+class ItemList(Resource):
+    def get(self):
+        return items
