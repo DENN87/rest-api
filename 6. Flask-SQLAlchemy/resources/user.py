0fcc2e25fb114c043d379b6aa0ec637061573bd6
DENN87 <denis.schiopu@yahoo.com>
2022-03-25 08:06:55 -0700

    Created User and Item Models.

--- 6. Flask-SQLAlchemy/resources/user.py
+++ 6. Flask-SQLAlchemy/resources/user.py
@@ -1,51 +1,6 @@
 import sqlite3
-
 from flask_restful import Resource, reqparse
-
-
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id,
-        self.username = username,
-        self.password = password
-    
-    # retrieve user object by username
-    @classmethod
-    def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))  # always a tuple with 2nd arg missing (arg1,)
-        # getting the first row
-        row = result.fetchone()
-        # if there is a row
-        if row:
-            # creating a new User object with the data from row
-            user = cls(*row)  # *row = row[0], row[1], row[2] = columns id, username, password
-        else:
-            user = None
-        
-        connection.close()
-        return user
-    
-    # retrieve user object by id
-    @classmethod
-    def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        
-        connection.close()
-        return user
-
+from models.user import UserModel
 
 class UserRegister(Resource):
     parser = reqparse.RequestParser()
@@ -63,7 +18,7 @@ class UserRegister(Resource):
     def post(self):
         data = UserRegister.parser.parse_args()
         
-        if User.find_by_username(data['username']):
+        if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists."}, 400
         
         connection = sqlite3.connect('data.db')
