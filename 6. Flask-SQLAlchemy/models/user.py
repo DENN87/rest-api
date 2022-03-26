1d0912e6c479930246ff6aa6980ec3591d0ad889
DENN87 <denis.schiopu@yahoo.com>
2022-03-26 06:47:47 -0700

    Implemented UserModel using SQLAlchemy.

--- 6. Flask-SQLAlchemy/models/user.py
+++ 6. Flask-SQLAlchemy/models/user.py
@@ -1,4 +1,3 @@
-import sqlite3
 from db import db
 
 class UserModel(db.Model):
@@ -8,45 +7,21 @@ class UserModel(db.Model):
     username = db.Column(db.String(80))
     password = db.Column(db.String(80))
     
-    def __init__(self, _id, username, password):
-        self.id = _id,
+    def __init__(self, username, password):
         self.username = username
         self.password = password
+    
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.close()
     
     # retrieve user object by username
     @classmethod
     def find_by_username(cls, username):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "SELECT * FROM users WHERE username=?"
-        result = cursor.execute(query, (username,))  # always a tuple with 2nd arg missing (arg1,)
-        # getting the first row
-        row = result.fetchone()
-        # if there is a row
-        if row:
-            # creating a new User object with the data from row
-            user = cls(*row)  # *row = row[0], row[1], row[2] = columns id, username, password
-        else:
-            user = None
-        
-        connection.close()
-        return user
+        return cls.query.filter_by(username=username).first()  # SELECT * FROM __tablename__(items) WHERE name=name
     
     # retrieve user object by id
     @classmethod
     def find_by_id(cls, _id):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-        
-        query = "SELECT * FROM users WHERE id=?"
-        result = cursor.execute(query, (_id,))
-        row = result.fetchone()
-        if row:
-            user = cls(*row)
-        else:
-            user = None
-        
-        connection.close()
-        return user
+        return cls.query.filter_by(id=_id).first()
 
