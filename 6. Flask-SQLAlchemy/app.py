f39ab32c03bed25c278cee6339a0e7a1a01180b5
DENN87 <denis.schiopu@yahoo.com>
2022-03-30 17:51:38 -0700

    Implemented logging in with Flask-JWT-Extended.

--- 6. Flask-SQLAlchemy/app.py
+++ 6. Flask-SQLAlchemy/app.py
@@ -1,22 +1,21 @@
 import os
 from flask import Flask, jsonify
 from flask_restful import Api
-from flask_jwt import JWT
-from security import authenticate, identity
-from resources.user import User, UserRegister
+from flask_jwt_extended import JWTManager
+
+from resources.user import User, UserLogin, UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 from db import db
 
 
 app = Flask(__name__)
-app.secret_key = 'SECRET_KEY'
-api = Api(app)
-
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-app.config['JWT_AUTH_URL_RULE'] = '/login'  # changing the url to the auth endpoint /login
 app.config['PROPAGATE_EXCEPTIONS'] = True
+
+app.secret_key = 'SECRET_KEY'
+api = Api(app)
 
 
 @app.before_first_request
@@ -24,31 +23,16 @@ def create_tables():
     db.create_all()
 
 
-jwt = JWT(app, authenticate, identity)  # creating the JWT instance, by default: /auth
-
-# customize JWT auth response, include user_id in response body
-@jwt.auth_response_handler
-def customized_response_handler(access_token, identity):
-    return jsonify({
-        'access_token': access_token.decode('utf-8'),
-        'user_id': identity.id
-        })
-
-# customize JWT auth response, include user_id in response body
-@jwt.jwt_error_handler
-def customized_error_handler(error):
-    return jsonify({
-                       'message': error.description,
-                       'code': error.status_code
-                   }), error.status_code
+jwt = JWTManager(app)
 
 
 api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
-api.add_resource(StoreList, '/stores')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserRegister, '/register')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     db.init_app(app)
--- 6. Flask-SQLAlchemy/resources/user.py
+++ 6. Flask-SQLAlchemy/resources/user.py
@@ -1,22 +1,26 @@
-import sqlite3
+import hmac
+
+from flask_jwt_extended import create_access_token, create_refresh_token
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                          type = str,
+                          required = True,
+                          help = "This field cannot be blank."
+                          )
+_user_parser.add_argument('password',
+                          type = str,
+                          required = True,
+                          help = "This field cannot be blank."
+                          )
+
+
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type = str,
-                        required = True,
-                        help = "This field cannot be blank."
-                        )
-    parser.add_argument('password',
-                        type = str,
-                        required = True,
-                        help = "This field cannot be blank."
-                        )
-    
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
         
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists."}, 400
@@ -34,7 +38,7 @@ class User(Resource):
         if not user:
             return {'message': 'User not found.'}, 404
         return user.json()
-
+    
     @classmethod
     def delete(cls, user_id):
         user = UserModel.find_by_id(user_id)
@@ -42,7 +46,24 @@ class User(Resource):
             return {'message': 'User not found.'}, 404
         user.delete_from_db()
         return {'message': 'User was deleted.'}, 200
+
+
+class UserLogin(Resource):
+    def post(self):
+        data = _user_parser.parse_args()
         
+        user = UserModel.find_by_username(data['username'])
+        
+        if user and hmac.compare_digest(user.password, data['password']):
+            access_token = create_access_token(identity = user.id, fresh = True)
+            refresh_token = create_refresh_token(user.id)
+            return {
+                       'access_token': access_token,
+                       'refresh_token': refresh_token
+                       }, 200
+        
+        return {'message': 'Invalid credentials.'}, 401
+
 
 # TO IMPLEMENT FOR ADMIN USER ONLY
 # from flask_jwt import jwt_required, current_identity
