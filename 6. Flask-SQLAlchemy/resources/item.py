a8ee36c6c206e4a15ebec6fd5608a300d22143cb
DENN87 <denis.schiopu@yahoo.com>
2022-03-30 21:56:31 -0700

    Getting JWT identity in at endpoint and Optional=True

--- 6. Flask-SQLAlchemy/resources/item.py
+++ 6. Flask-SQLAlchemy/resources/item.py
@@ -1,4 +1,4 @@
-from flask_jwt_extended import get_jwt, jwt_required
+from flask_jwt_extended import get_jwt, get_jwt_identity, jwt_required
 from flask_restful import Resource, reqparse
 from models.item import ItemModel
 
@@ -58,6 +58,15 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_required(optional=True)
     def get(self):
+        user_id = get_jwt_identity()
         # or list Comprehension [x.json() for x in ItemModel.query.all()]
-        return {'items': list(map(lambda x: x.json(), ItemModel.find_all()))}
+        items = list(map(lambda item: item.json(), ItemModel.find_all()))
+        if user_id:
+            return {'items': items}, 200
+        
+        return {
+            'items': [item['name'] for item in items],
+            'message': 'More data available if you log in.'
+            }, 200
