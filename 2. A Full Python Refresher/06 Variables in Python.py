fc0d89f1ae98342f7a5f7c4714feb35c3f02887a
DENN87 <denis.schiopu@yahoo.com>
2022-03-31 19:02:26 -0700

    06 Variables in Python

---
+++ 2. A Full Python Refresher/06 Variables in Python.py
@@ -0,0 +1,36 @@
+"""
+Variables in Python
+"""
+x = 15
+price = 9.99  # float value
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result)
+
+
+"""
+Strings in Python
+"""
+name = "Rolf"
+
+print(name)
+print(name * 2)  # name + name (join 2 strings)
+
+"""
+Changing variables values
+"""
+a = 25
+b = a
+
+print(a)
+print(b)
+
+b = 17
+
+print(a)
+print(b)
+
+
+
