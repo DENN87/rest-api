9a2d12789248701bc95bd76c8e1df7c622043b79
DENN87 <denis.schiopu@yahoo.com>
2022-03-31 20:40:53 -0700

    The 'in' keyword

---
+++ 2. A Full Python Refresher/09_The 'in' keyword.py
@@ -0,0 +1,10 @@
+"""
+the 'in' keyword
+"""
+
+friends = ["Rolf", "Bob", "Jen"]
+print("Jen" in friends)
+
+movies_watched = {"The Matrix", "Green Lantern", "Flash"}
+user_movie = input("Enter something you've watched recently: ")
+print(user_movie in movies_watched)
