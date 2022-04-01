e6a5f62ae6405a6b72cf7f383d04029fc2010302
DENN87 <denis.schiopu@yahoo.com>
2022-03-31 19:08:02 -0700

    07 Solution to coding Exercise_Variables

---
+++ 2. A Full Python Refresher/07 Exercise_Variables.py
@@ -0,0 +1,14 @@
+"""
+1. Create two variables, var1 and var2, with any value. It can be anything.
+2. Create two variables, num1 and num2, that multiply together to give 16
+"""
+
+var1 = "Hello,"
+var2 = "Python!"
+
+print(var1 + ' ' + var2)
+
+num1 = 4
+num2 = 4
+
+print(num1 * num2)
