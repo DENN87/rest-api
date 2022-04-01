758809b1ce05a6f7a8c0ffca5b50308eca3479ef
DENN87 <denis.schiopu@yahoo.com>
2022-03-31 20:37:01 -0700

    If statements

---
+++ 2. A Full Python Refresher/08_if statements.py
@@ -0,0 +1,15 @@
+"""
+if statements
+"""
+
+day_of_week = input("What day of the week is it today? ").lower()
+
+if day_of_week == "monday":
+    print("Have a great start of your week!")
+elif day_of_week == "saturday" or "sunday":
+    print("Enjoy your weekend :)")
+else:
+    print("Your weekend is coming fast!")
+
+    
+print("This always run.")
