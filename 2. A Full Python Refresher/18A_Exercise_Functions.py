3de446e4ed3988654f08d5c8fba70602a13aeb3e
DENN87 <denis.schiopu@yahoo.com>
2022-04-11 10:25:30 -0700

    Functions Exercise

---
+++ 2. A Full Python Refresher/18A_Exercise_Functions.py
@@ -0,0 +1,20 @@
+"""
+Functions Exercise:
+1. Complete the function 'return_42', by making it 'return 42'
+2. Create a function, which must be called 'my_function', which takes two arguments
+and returns the result of its two arguments multiplied together.
+"""
+
+# 1
+def return_42():
+    return 42
+
+# 2
+def my_function(x, y):
+    return x * y
+
+
+print(return_42())
+
+print(my_function(2, 2))
+
