e33a394b77b9095c29736573b87aaeb4aa55ae6d
DENN87 <denis.schiopu@yahoo.com>
2022-04-11 20:14:08 -0700

    Custom error classes

---
+++ 2. A Full Python Refresher/31_Custom error classes.py
@@ -0,0 +1,33 @@
+"""
+Custom error classes
+"""
+
+
+class TooManyPagesReadError(ValueError):  # inheritance from ValueError
+    pass
+
+
+class Book:
+    def __init__(self, name: str, page_count: int):
+        self.name = name
+        self.page_count = page_count
+        self.pages_read = 0
+
+    def __repr__(self):
+        return f"<Book {self.name}, read {self.pages_read} pages out of {self.page_count}>"
+
+    def read(self, pages: int):
+        if self.pages_read + pages > self.page_count:
+            raise TooManyPagesReadError(
+                f"You tried to read {self.pages_read + pages} pages, but this book only has {self.page_count} pages."
+            )
+        self.pages_read += pages
+        print(f"You have now read {self.pages_read} pages out of {self.page_count}")
+
+
+python_basics = Book("Python Basics", 50)
+try:
+    python_basics.read(35)
+    python_basics.read(50)
+except TooManyPagesReadError as error:
+    print(error)
